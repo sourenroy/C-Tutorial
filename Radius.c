28692a470f1fa3c95287e3839169ba6f65702c21
sourenroy <168115446+sourenroy@users.noreply.github.com>
2025-09-04 22:42:17 +0530

    Update Radius.c

--- Radius.c
+++ Radius.c
@@ -5,6 +5,7 @@ int main(){
     float radius ;
     printf("enter radius ;");
     scanf("%f", &radius);
+    //Value of pi = 3.14 and the formula is 3.14 * radius of circle * radius of circle 
     printf("area is %f", 3.14 * radius * radius);
 
 return 0;
