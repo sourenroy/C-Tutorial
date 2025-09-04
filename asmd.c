86fdc502688dda5a84bebff23cbc26abca19bccd
sourenroy <168115446+sourenroy@users.noreply.github.com>
2025-09-04 22:43:41 +0530

    Update asmd.c

--- asmd.c
+++ asmd.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
-void main(){
-int a,b,c,d,e,f,g;
+void main()
+{
+    int a,b,c,d,e,f,g;
+
 printf("Enter two num :");
 scanf("%d%d",&a,&b);
-c=a+b;
+    c=a+b;
 printf("The sum is %d \n",c);
-d=a-b;
+    d=a-b;
 printf("The substraction is %d\n",d);
-e=a*b;
+    e=a*b;
 printf("The multiplication is %d\n",e);
-f=a/b;
+    f=a/b;
 printf("The division is %d\n",f);
 };
