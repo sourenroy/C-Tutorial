d179c47c2c9535151e9d644ef41c9ccea35b78da
sourenroy <168115446+sourenroy@users.noreply.github.com>
2025-09-07 17:09:54 +0530

    insurance.c

--- insurance.c
+++ insurance.c
@@ -1,27 +1,28 @@
 #include<stdio.h>
+
 void main(){
 int age;
 char g,ms,f,m,u;
-printf("\nenter the age");
-scanf("%d",&age);
-printf("\nenter the gender");
-scanf("%c",&g);
-printf("\nenter maritial status");
-scanf("%c",&ms);
+     printf("\nenter the age");
+     scanf("%d",&age);
+     printf("\nenter the gender");
+     scanf("%c",&g);
+     printf("\nenter maritial status");
+     scanf("%c",&ms);
 if (ms=='m')
-{
-printf("eligble for insurance");
-}
+     {
+     printf("eligble for insurance");
+     }
 else if (ms=='u'&& g=='f' && age>30)
- {
-printf("eligble for insurance");
-}
+     {
+     printf("eligble for insurance");
+     }
 else if (ms=='u' && g=='f'&&age>25)
-{
-printf("eligble for insurance");
-}
+     {
+     printf("eligble for insurance");
+     }
 else
-{
-printf("NOT eligble for insurance");
-}
+     {
+     printf("NOT eligble for insurance");
+     }
 }
