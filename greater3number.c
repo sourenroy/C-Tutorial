02df6026d1e823ecbb75245b08ce569b210d1159
sourenroy <168115446+sourenroy@users.noreply.github.com>
2025-09-07 17:08:44 +0530

    greater3number.c

--- greater3number.c
+++ greater3number.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
+
 void main(){
-int a,b,c;
-printf("enter 3 number");
-scanf("%d%d%d",&a,&b,&c);
-if(a>b&&a>c)
-{
-printf("THE GREATER NUM IS%d",a);
-}
-else if(b>c&&b>a)
-{
-printf("THE GREATER NUM IS%d",b);
-}
-else
-{
-printf("THE GREATER NUM IS%d",c);
-}
+    int a,b,c;
+  
+      printf("enter 3 number");
+      scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+      {
+      printf("THE GREATER NUM IS%d",a);
+      }
+    else if(b>c&&b>a)
+      {
+      printf("THE GREATER NUM IS%d",b);
+      }
+    else
+      {
+      printf("THE GREATER NUM IS%d",c);
+      }
 }
