ea3c804fefcd5269aad62871367358e39c9cf9f1
sourenroy <168115446+sourenroy@users.noreply.github.com>
2025-09-08 08:19:58 +0530

    nested.c

--- nested.c
+++ nested.c
@@ -5,21 +5,24 @@ int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
 if(a>b)
 {
-if(a>c){
-    printf("greatest num is %d",a);
-}
-else{
-    printf("greatest num is %d",c);
-}
-}
-else
-{
-if(b>c){
-    printf("greates num is %d",b);
+        if(a>c)
+        {
+            printf("greatest num is %d",a);
+        }
+        else
+        {
+            printf("greatest num is %d",c);
+        }
 }
 else
 {
-    printf("greatest num is %d",c);
-}
+        if(b>c)
+        {
+            printf("greates num is %d",b);
+        }
+        else
+        {
+            printf("greatest num is %d",c);
+        }
 }
 }
