d6f3612adae49c94d3ea73addce55be310fb0106
sourenroy <168115446+sourenroy@users.noreply.github.com>
2025-09-08 08:17:22 +0530

    leapyear2.c

--- leapyear2.c
+++ leapyear2.c
@@ -4,19 +4,19 @@ void main(){
         printf("enter the year");
         scanf("%d",&n);
     if (n%400 ==0)
-    {
+        {
         printf("THIS IS A LEAPYEAR%d",n);
-    }
+        }
     else if(n%100==0)
-    {
+        {
         printf("THIS IS A NOT LEAPYEAR%d",n);
-    }
+        }
     else if(n%4==0)
-    {
+        {
         printf("THIS IS A LEAPYEAR%d",n);
-    }
+        }
     else 
-    {
+        {
         printf("THIS IS not A LEAPYEAR%d",n);
-    }
- }
+        }
+ }    
